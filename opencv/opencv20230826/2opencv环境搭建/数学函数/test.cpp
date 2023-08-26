640b67d160b32a183ff132e41f56c5e090a03da5
ityege <limou6233@gmail.com>
2023-08-27 07:49:59 +0800

    opencv 数学函数

---
+++ opencv/opencv20230826/2opencv环境搭建/数学函数/test.cpp
@@ -0,0 +1,17 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cout << "cvRound(2.5) : " << cvRound(2.5) << endl;
+	cout << "cvRound(2.6) : " << cvRound(2.6) << endl;
+	cout << "cvRound(2.8) : " << cvRound(2.8) << endl;
+
+	cout << "cvFloor(2.5) : " << cvFloor(2.5) << endl;
+	cout << "cvFloor(2.6) : " << cvFloor(2.6) << endl;
+	cout << "cvCeil(2.5)  : " << cvCeil(2.5) << endl;
+	cout << "cvCeil(2.6)  : " << cvCeil(2.6) << endl;
+	return 0;
+}

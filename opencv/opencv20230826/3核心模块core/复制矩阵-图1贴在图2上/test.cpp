dd2227226f42112b265b9783d0d1b454273bc089
ityege <limou6233@gmail.com>
2023-09-09 18:02:20 +0800

    矩阵复制 将图1贴到图2上面

---
+++ opencv/opencv20230826/3核心模块core/复制矩阵-图1贴在图2上/test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;  //所有opencv类都在命名空间cv下
+using namespace std;
+
+
+int main()
+{
+	Mat Image1 = imread("520.jpg");
+	Mat Image2 = imread("2.jpg");
+	imshow("图1原始图", Image1);
+	imshow("图2原始图", Image2);
+	Mat imagedst = Image1(Rect(0, 0, Image2.cols, Image2.rows));
+	Mat mask = imread("2.jpg", 0);
+	imshow("mask", mask);
+	//进行复制
+	Image2.copyTo(imagedst, mask);
+	imshow("效果图为", Image1);
+	waitKey(0);
+	return 0;
+}

d7b398f136f40f15fd50b917c6ee1a4da2aeeb75
ityege <limou6233@gmail.com>
2023-09-09 18:02:37 +0800

    矩阵遍历 数组指针方式

---
+++ opencv/opencv20230826/3核心模块core/矩阵的遍历-指针数组/test.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;  //所有opencv类都在命名空间cv下
+using namespace std;
+
+
+int main()
+{
+	Mat mymat = cv::Mat::ones(cv::Size(3, 2), CV_8UC3);//定义2行3列3通道矩阵,实际矩阵公有9列
+	uchar* pdata = (uchar*)mymat.data;
+
+	for (int i = 0; i < mymat.rows; i++)
+	{
+		for (int j = 0; j < mymat.cols * mymat.channels(); j++)
+			cout << (int)pdata[j] << "   ";
+		cout << endl;
+	}
+}
+
+

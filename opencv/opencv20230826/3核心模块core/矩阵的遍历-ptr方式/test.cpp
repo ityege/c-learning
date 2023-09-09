d38e2a1832f81007ed1465c1fc6aa0b154d1f87d
ityege <limou6233@gmail.com>
2023-09-09 18:05:01 +0800

    矩阵的遍历 .ptr方式

---
+++ opencv/opencv20230826/3核心模块core/矩阵的遍历-ptr方式/test.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;  //所有opencv类都在命名空间cv下
+using namespace std;
+
+
+int main()
+{
+	Mat mymat = cv::Mat::ones(cv::Size(3, 2), CV_8UC3);//定义2行3列3通道矩阵,实际矩阵公有9列
+	for (int i = 0; i < mymat.rows; i++)
+	{
+		uchar* pdata = mymat.ptr<uchar>(i);
+		for (int j = 0; j < mymat.cols * mymat.channels(); j++)
+			cout << (int)pdata[j] << "   ";
+		cout << endl;
+	}
+}
+
+

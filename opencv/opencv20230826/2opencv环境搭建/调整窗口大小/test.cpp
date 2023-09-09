0cc8652a3fd313946c9b3089f85ba699de64a8c8
ityege <limou6233@gmail.com>
2023-09-09 16:26:29 +0800

    调整窗口大小

---
+++ opencv/opencv20230826/2opencv环境搭建/调整窗口大小/test.cpp
@@ -0,0 +1,29 @@
+﻿
+#include "pch.h"
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2\opencv.hpp>
+#pragma comment(lib, "opencv_world450d.lib")  //引用引入库 
+using namespace cv;
+#include<iostream>
+using namespace std;
+ 
+int main(){
+	vector<Mat>srcImage(1);
+	char szName[50] = "";
+	int  width = 240, height = 120;
+	
+	sprintf_s(szName, "%d.jpg", 1);
+	srcImage[0] = imread(szName);
+	if (srcImage[0].empty()) {  
+		cout << "read " << szName << " error" << endl;
+		return -1;
+	}
+	namedWindow(szName, WINDOW_NORMAL);
+	imshow(szName, srcImage[0]);
+	resizeWindow(szName, width, height);
+ 
+	waitKey(0);
+	system("pause");
+
+	return 0;
+}

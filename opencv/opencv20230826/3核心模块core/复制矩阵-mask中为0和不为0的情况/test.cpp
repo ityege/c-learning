0d265f73a5eea32e13c7581ad98cdc5e5e681c76
ityege <limou6233@gmail.com>
2023-09-09 18:01:36 +0800

    矩阵复制 mask为0和不为0的情况

---
+++ opencv/opencv20230826/3核心模块core/复制矩阵-mask中为0和不为0的情况/test.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;  //所有opencv类都在命名空间cv下
+using namespace std;
+
+
+int main()
+{
+	Mat image, mask;
+	Rect r1(10, 10, 60, 100);
+	Mat img2;
+	image = imread("520.jpg");
+	mask = Mat::zeros(image.size(), CV_8UC1);
+	mask(r1).setTo(255);
+	image.copyTo(img2, mask);
+	imshow("原图像", image);
+	imshow("复制后的目标图像", img2);
+	imshow("mask", mask);
+	waitKey();
+
+	return 0;
+}

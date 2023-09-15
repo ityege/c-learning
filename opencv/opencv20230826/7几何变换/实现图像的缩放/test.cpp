ee3bd635c9d9a568f1e02a16eeac0d700e848727
ityege <limou6233@gmail.com>
2023-09-15 19:27:55 +0800

    实现图像的缩放

---
+++ opencv/opencv20230826/7几何变换/实现图像的缩放/test.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	// 读取图片
+	Mat srcImage = imread("djy.jpg");
+
+	if (!srcImage.data)   //Check for invalid input
+	{
+		cout << "Could not open or find the image" << endl;
+		return -1;
+	}
+
+	Mat dstImage1, dstImage2;
+	Mat tempImage = srcImage;
+	// 显示图片 
+	imshow("原图", srcImage);
+
+	// 图片的缩小
+	resize(tempImage, dstImage1, Size(tempImage.cols / 2, tempImage.rows / 2), 0, 0, INTER_NEAREST);
+	// 图片的放大
+	resize(tempImage, dstImage2, Size(tempImage.cols * 2, tempImage.rows * 2), 0, 0, INTER_NEAREST);
+
+	imshow("缩小图", dstImage1);
+	imshow("放大图", dstImage2);
+
+	waitKey();
+	return 0;
+}

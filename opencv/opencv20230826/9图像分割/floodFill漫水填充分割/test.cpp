9b3bd969b31c88035ae8cec2f5f7e9e368abdcf7
ityege <limou6233@gmail.com>
2023-09-16 17:46:19 +0800

    floodFill漫水填充分割

---
+++ opencv/opencv20230826/9图像分割/floodFill漫水填充分割/test.cpp
@@ -0,0 +1,16 @@
+﻿ #include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/imgproc/types_c.h>
+#include <iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+	Mat src = imread("test.jpg");
+	imshow("【原始图】", src);
+	Rect ccomp;
+	floodFill(src, Point(50, 300), Scalar(155, 255, 55), &ccomp, Scalar(20, 20, 20), Scalar(20, 20, 20));
+	imshow("【效果图】", src);
+	waitKey(0);
+	return 0;
+}

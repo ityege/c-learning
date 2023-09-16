6160cc02444e73efc2f21d7621c0706615f6fd56
ityege <limou6233@gmail.com>
2023-09-16 17:16:29 +0800

    固定阈值和自适应阈值分割对比

---
+++ opencv/opencv20230826/9图像分割/固定阈值和自适应阈值分割对比/test.cpp
@@ -0,0 +1,23 @@
+﻿#include <opencv2/core/core.hpp>              
+#include <opencv2/highgui/highgui.hpp>              
+#include <opencv2/imgproc/imgproc.hpp>                
+#include <opencv2\imgproc\types_c.h>
+
+using namespace cv;
+int main()
+{
+	Mat img = imread("test.jpg");
+	Mat dst1;
+	Mat dst2;
+	Mat dst3;
+	cv::cvtColor(img, img, COLOR_RGB2GRAY);//进行，灰度处理
+	medianBlur(img, img, 5);//中值滤波
+	threshold(img, dst1, 127, 255, THRESH_BINARY);//阈值分割
+	adaptiveThreshold(img, dst2, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 11, 2);//自动阈值分割,邻域均值
+	adaptiveThreshold(img, dst3, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, 2);//自动阈值分割，高斯邻域
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+	imshow("dst3", dst3);
+	imshow("img", img);
+	waitKey(0);
+}

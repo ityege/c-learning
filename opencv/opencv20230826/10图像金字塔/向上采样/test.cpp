76127a910ac41b382ac1d52d7d0ec557990596ac
ityege <limou6233@gmail.com>
2023-09-17 13:31:30 +0800

    向下采样

---
+++ opencv/opencv20230826/10图像金字塔/向上采样/test.cpp
@@ -0,0 +1,22 @@
+﻿#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+
+int main()
+{
+	Mat img, r1, r2;
+	img = imread("img.jpg", IMREAD_COLOR); 	//读取原始图像
+	pyrDown(img, r1);  	//图像向下取样
+	pyrUp(r1, r2);
+
+	imshow("original", img);  	//显示图像
+	imshow("PyrDown1", r1);
+	imshow("PyrUp", r2);
+
+	waitKey(0);
+	destroyAllWindows(); //销毁所有窗口
+}

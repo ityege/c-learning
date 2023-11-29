813af73da1b8ae773dc6de176fd135d0028bf62b
ityege <limou6233@gmail.com>
2023-11-29 21:26:18 +0800

    算数仿函数

---
+++ stl/大道至简20231122/13仿函数/1306算数仿函数/1362求余仿函数和求反仿函数.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+using namespace std;
+void myprint(vector<int> &vd)
+{
+    vector<int>::iterator it;
+    for (it = vd.begin(); it != vd.end(); it++)
+        cout << *it << " , ";
+    cout << endl;
+}
+int main(int argc, char *argv[])
+{
+    const int N = 6;
+    vector<int> V(N), V2(N), V3(N);
+    generate(V.begin(), V.end(), rand);
+    cout << "vector v: " << endl;
+    myprint(V);
+    transform(V.begin(), V.end(), V2.begin(), bind2nd(modulus<int>(), 10));
+    cout << "vector v2: " << endl;
+    myprint(V2);
+    transform(V.begin(), V.end(), V3.begin(), negate<int>());
+    cout << "vector v3: " << endl;
+    myprint(V3);
+    return 0;
+}

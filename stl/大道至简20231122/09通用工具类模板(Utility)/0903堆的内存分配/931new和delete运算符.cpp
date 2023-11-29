53a557017a35001a51ac5e54da88312ec23015b1
ityege <limou6233@gmail.com>
2023-11-29 21:22:17 +0800

    堆的内存分配

---
+++ stl/大道至简20231122/09通用工具类模板(Utility)/0903堆的内存分配/931new和delete运算符.cpp
@@ -0,0 +1,20 @@
+#include <memory>
+#include <iostream>
+using namespace std;
+struct Date
+{
+    int month;
+    int day;
+    int year;
+};
+int main()
+{
+    Date *birthday = new Date;
+    birthday->month = 6;
+    birthday->day = 24;
+    birthday->year = 2012;
+    cout << "This day is : " << birthday->month << "/" << birthday->day << "/" << birthday->year;
+    cout << endl;
+    delete birthday;
+    return 0;
+}
